e3bb200c026ba24b83fd78ba31a970a0887ed167
EbichanOO <takanori.sao@icloud.com>
2021-05-28 03:33:15 +0900

    [add]concat some pdf function

--- tools/concatPdf.py
+++ tools/concatPdf.py
@@ -8,5 +8,13 @@ def ConcatPdfTwo(A_pdf_path1, A_pdf_path2, out_path_and_name):
     merger.write(out_path_and_name)
     merger.close()
 
+def ConcatPdfSome(pdf_paths, out_path_and_name):
+    merger = PyPDF2.PdfFileMerger()
+    for A_pdf_path in pdf_paths:
+        merger.append(A_pdf_path)
+    merger.setPageLayout("/SinglePage")
+    merger.write(out_path_and_name)
+    merger.close()
+
 if __name__ == '__main__':
-    ConcatPdfTwo("./tmps/A.pdf", "./tmps/B.pdf", "./tmps/C.pdf")
+    ConcatPdfSome(["./tmps/A.pdf", "./tmps/B.pdf", "./tmps/C.pdf"], "./tmps/D.pdf")
