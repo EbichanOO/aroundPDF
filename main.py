34071fa2bc5581e3fd5f36a2940058e68a0a8bed
EbichanOO <mititaka.mail@gmail.com>
2022-11-07 23:42:41 +0900

    動かなくなった

--- main.py
+++ main.py
@@ -7,27 +7,61 @@ from typing_extensions import IntVar
 import tools
 import controller
 
+width = 300
+height = 300
+
+class MainMenu():
+    def __init__(self):
+        self.topMenu = TopMenu(root)
+        self.frame = ttk.Frame(root)
+        self.frame.grid(row=1, column=0, sticky="nsew")
+        self.frames = [self.topMenu.frame, self.frame]
+
+    def places(self):
+        self.topMenu.places()
+
+        ITP = ImgToPdf()
+        CPDF = ConcatPdf()
+
+        # 各種ウィジェットの作成
+        button_change = button_default(self.frame, text="画像pdf変換", command=self.change_to_imgToPdf)
+        butoon_change_concatPdf = button_default(self.frame, text="pdf結合", command=CPDF.change_concatPdf)
+
+        # 各種ウィジェットの設置
+        button_change.pack()
+        butoon_change_concatPdf.pack()
+    
+    def change(self):
+        [frame.tkraise() for frame in self.frames]
+        self.places()
+
+    def change_to_imgToPdf(self):
+        [frame.destroy() for frame in self.frames]
+        ITP = ImgToPdf()
+        ITP.change_imgToPdf()
+
+
 class ImgToPdf:
     def __init__(self):
         self.filenames = []
         # アプリフレームの作成と設置
         self.frame_imgToPdf = ttk.Frame(root)
-        self.frame_imgToPdf.grid(row=0, column=0, sticky="nsew")
+        self.frame_imgToPdf.grid(row=1, column=0, sticky="nsew")
 
-        self.back_img = tk.PhotoImage(file='./imgs/back.png')
-        self.back_img = self.back_img.subsample(10,10)
+        self.top_menu = TopMenu(root)
+
+        self.frames = [self.frame_imgToPdf, self.top_menu.frame]
     
     def places(self):
         # back image
         
-        back_button = ttk.Button(self.frame_imgToPdf, image=self.back_img, command=change_main)
+        self.top_menu.places()
 
         label1_frame_imgToPdf = ttk.Label(self.frame_imgToPdf, text="画像pdf変換")
-        button_get_folder_path = button_default(self.frame_imgToPdf, text="フォルダで一括選択", command=ITP.get_folderpath)
-        button_get_img_path = button_default(self.frame_imgToPdf, text="画像またはzipファイルを選択", command=ITP.get_imgpath)
+        button_get_folder_path = button_default(self.frame_imgToPdf, text="フォルダで一括選択", command=self.get_folderpath)
+        button_get_img_path = button_default(self.frame_imgToPdf, text="画像またはzipファイルを選択", command=self.get_imgpath)
         self.img_label = ttk.Label(self.frame_imgToPdf, text="0個の画像を選択")
         
-        back_button.pack(side="left", anchor="nw")
         label1_frame_imgToPdf.pack()
         button_get_folder_path.pack()
         button_get_img_path.pack()
@@ -47,10 +81,11 @@ class ImgToPdf:
 
         self.pgb = ttk.Progressbar(self.frame_imgToPdf,orient="horizontal",value=0,maximum=100,length=200,mode='determinate')
         self.pgb.pack()
-        
+
     def change_imgToPdf(self):
-        self.clear_imgpath()
-        self.frame_imgToPdf.tkraise()
+        #self.clear_imgpath()
+        [name.tkraise() for name in self.frames]
+        self.places()
 
     def get_imgpath(self):
         filenames_tmp = filedialog.askopenfilenames(filetypes = [("","*")])
@@ -164,6 +199,31 @@ def write_log(log_board, text):
     log_board.insert(tk.END, text+"\n")
     log_board.configure(state='disable')
 
+class TopMenu:
+    def __init__(self, parentFrame):
+        self.frame_height = int(height/5)
+        self.frame_width = width
+
+        self.frame = tk.Frame(parentFrame, width=width, height=self.frame_height)
+        self.style = ttk.Style()
+        self.style.configure("topMenu.TButton")
+
+        self.img_width = 5*self.frame_height
+        self.img_height = self.frame_height
+        self.logo_img = tk.PhotoImage(file='./imgs/my-toolkit-logo.png', width=self.img_width, height=self.frame_height) #size 500 100
+
+        self.canvas = tk.Canvas(self.frame, width=self.img_width, height=self.frame_height)
+        #self.logo_img = self.logo_img.subsample(4,4)
+
+    def places(self):
+        self.frame.grid(column=0,row=0)
+        self.canvas.pack()
+        # キャンバスにイメージを表示
+        self.canvas.create_image(0, 0, image=self.logo_img, anchor=tkinter.NW)
+        self.canvas.bind('go top', change_main)
+        #logo = tk.Button(self.frame, image=self.logo_img, command=change_main)
+        #logo.pack()
+
 if __name__ == "__main__":
     root = tk.Tk()
     global main_color, normal_font
@@ -176,30 +236,15 @@ if __name__ == "__main__":
 
     # rootメインウィンドウの設定
     root.title("toolkit")
-    root.geometry("300x300")
+    root.geometry("{}x{}".format(width,height))
     root.grid_rowconfigure(0, weight=1)
     root.grid_columnconfigure(0, weight=1)
     root.configure(bg=main_color) # background color
 
-    ITP = ImgToPdf()
-    CPDF = ConcatPdf()
+    MAINMENU = MainMenu()
 
-    # メインフレームの作成と設置
-    frame = ttk.Frame(root)
-    frame.grid(row=0, column=0, sticky="nsew")
+    MAINMENU.places()
 
-    # 各種ウィジェットの作成
-    button_change = button_default(frame, text="画像pdf変換", command=ITP.change_imgToPdf)
-    butoon_change_concatPdf = button_default(frame, text="pdf結合", command=CPDF.change_concatPdf)
-
-    # 各種ウィジェットの設置
-    button_change.pack()
-    butoon_change_concatPdf.pack()
-
-    ITP.places()
-    CPDF.places()
-
-    # frameを前面にする
-    frame.tkraise()
+    MAINMENU.change()
 
     root.mainloop()
